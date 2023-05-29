760f575e705957003f3d47f7c7a8c501e4be3335
MrSu18 <3051605203@qq.com>
2023-05-29 14:38:46 +0800

    初步完成了客户端通过用户名和密码进行登陆

--- client.py
+++ client.py
@@ -1,11 +1,21 @@
 import socket
 
 client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# 给服务器发送请求登陆的信号
+msg = '请求登陆'.encode('UTF-8')
+sent = client.sendto(msg, ('localhost', 10000))
+data, server = client.recvfrom(4096)
+print(data.decode())
+# 输入用户名发送给服务器查询
+user_name=input().encode('UTF-8')
+client.sendto(user_name, ('localhost', 10000))
+data, server = client.recvfrom(4096)
+print(data.decode())
+# key
+password=input().encode('UTF-8')
+client.sendto(password, ('localhost', 10000))
+data, server = client.recvfrom(4096)
+print(data.decode())
 
-try:
-    msg = "发送消息到服务器".encode('utf-8')
-    sent = client.sendto(msg, ('localhost', 10000)) #socket.sento(msg,(ip,port))
-    data, server = client.recvfrom(4096)
-    print(data.decode())
-finally:
-    client.close()
+# 关闭客户端
+client.close()
--- server.py
+++ server.py
@@ -3,12 +3,29 @@ import socket  # 导入 socket 模块
 
 class user:
     #定义一个用户对象，如果有用户登陆则实例化一个
-    def __init__(self,name=0,pwd=0):
+    def __init__(self,name=0,pwd=0,address=(0,0)):
         self.name=name
         self.pwd=pwd
+        self.address=address
     def intro(self):
-        print(self.name,self.pwd)
+        print(self.name,self.pwd,self.address)
 
+def User_Login(address):
+    # 文本读写获取用户密码进行核对
+    server.sendto('请输入用户名:'.encode('UTF-8'), address)
+    data, address = server.recvfrom(1024)
+    user_name=data.decode('UTF-8')
+    file=open('UserAccount.txt','r')
+    for line in file: #把文件的每一行都给line
+        line=line.strip().split(',') #分割出[用户名，密码]的列表
+        if line[0]==user_name:
+            server.sendto('请输入密码:'.encode('UTF-8'), address)
+            data, address = server.recvfrom(1024)
+            key=data.decode('UTF-8')
+            if key==line[1]:
+                server.sendto('登陆成功'.encode('UTF-8'), address)
+                return 1
+    return 0
 
 # 配置服务器
 server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -18,8 +35,7 @@ print("服务器已开启，正在监听{}".format(server.getsockname()))
 while True:
     #接受客户端的数据
     data, address = server.recvfrom(1024)
-    print(data.decode('UTF-8'), address)
-    if data:
-        sent = server.sendto('已接收到你发来的消息'.encode('UTF-8'), address)
+    if data.decode('UTF-8')=='请求登陆':
+        User_Login(address)
 
 server.close()
